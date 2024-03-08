466faef33bb940b5747453547ef4a25eee8db5cb
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-03-08 23:52:09 +0400

    - FIxed cron example

--- examples/cron/main.tf
+++ examples/cron/main.tf
@@ -1,11 +1,28 @@
 
-# * Basic Example * #
-
+# * Cron Example * #
 module "lambda_simple" {
-  source         = "../../"
-  function_name  = "test-cron"
-  description    = "This is a test"
-  source_path    = "./function/"
-  cron           = "cron(0 12 * * ? *)"
-  schedule_group = "test-group-cron"
+  source            = "../../"
+  function_name     = "test-cron"
+  description       = "This is a test"
+  source_path       = "./function/"
+  cron              = "cron(0 12 * * ? *)"
+  schedule_group    = "test-group-cron"
+  schedule_role_arn = aws_iam_role.schedule_role.arn
+}
+
+# Schedule role
+resource "aws_iam_role" "schedule_role" {
+  name = "schedule_role_cron"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
