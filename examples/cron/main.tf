cc36f70546cf942af8f08ecd912b3d7c39338ca0
Mervin Hemaraju <115166315+mervinhemaraju@users.noreply.github.com>
2024-03-09 00:15:19 +0400

    - Added cron test (#2)
    * - Added cron test
    - Updated README
    
    * - Fixed examples
    
    * - FIxed cron example
    
    * - Fixed cron example
    
    * - Fixed outputs
    
    * - Fixed cron example

--- examples/cron/main.tf
+++ examples/cron/main.tf
@@ -1,11 +1,45 @@
 
-# * Basic Example * #
+# Schedule role
+resource "aws_iam_role" "schedule_role" {
+  name = "schedule_role_cron"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-module "lambda_simple" {
-  source         = "../../"
-  function_name  = "test"
-  description    = "This is a test"
-  source_path    = "./function/"
-  cron           = "cron(0 12 * * ? *)"
-  schedule_group = "test-group"
+# Managed scehdule group
+resource "aws_scheduler_schedule_group" "cron_test" {
+  name = "test-group-1"
+}
+
+
+# * Cron Example with schedule group * #
+module "lambda_simple_cron_1" {
+  source            = "../../"
+  function_name     = "test-cron-1"
+  description       = "This is a test"
+  source_path       = "./function/"
+  cron              = "cron(0 12 * * ? *)"
+  schedule_group    = aws_scheduler_schedule_group.cron_test.name
+  schedule_role_arn = aws_iam_role.schedule_role.arn
+}
+
+
+# * Cron Example without schedule group * #
+module "lambda_simple_cron_2" {
+  source            = "../../"
+  function_name     = "test-cron-2"
+  description       = "This is a test"
+  source_path       = "./function/"
+  cron              = "cron(0 12 * * ? *)"
+  schedule_role_arn = aws_iam_role.schedule_role.arn
 }
